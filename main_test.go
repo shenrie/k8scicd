802c6f20a4cf7a9f3db344d10badd7dd0c89835e
Stephen Henrie <sphenrie@gmail.com>
2020-03-31 15:50:21 -0700

    Updated text

--- main_test.go
+++ main_test.go
@@ -19,7 +19,7 @@ func TestServeHTTP(t *testing.T) {
 	if resp.StatusCode != 200 {
 		t.Fatalf("Received non-200 response: %d\n", resp.StatusCode)
 	}
-	expected := `{"message": "Hello World!!!"}`
+	expected := `{"message": "Hello World123!!!"}`
 	actual, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatal(err)
